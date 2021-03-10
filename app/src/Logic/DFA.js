a95b65d24fb91ef4363976c59c6ef5eb85fda6a9
AndrewOleynikov <a.oleyn1kov@outlook.com>
2021-03-10 22:22:53 +0300

    Graph representation from adjacency list to matrix

--- app/src/Logic/DFA.js
+++ app/src/Logic/DFA.js
@@ -1,33 +1,65 @@
 "use strict";
 exports.__esModule = true;
 exports.DFA = exports.eof = void 0;
-exports.eof = { id: -1, isAdmit: false };
+exports.eof = { isAdmit: false, idLogic: -1 };
 var DFA = /** @class */ (function () {
-    function DFA(statements, matrix, input, alphabet) {
+    function DFA(startStatement, graph, input /*statements: statement[], matrix: statement[][], input: elementOfAlphabet[] , alphabet: elementOfAlphabet[]*/) {
+        //this.statements = statements
+        //this.input = input
+        //this.matrix = matrix
+        //this.alphabet = alphabet
+        //this.startStatement = statements[0]
         var _this = this;
-        this.firstMatchValue = function (current) {
-            var i = 0;
-            while (current !== _this.alphabet[i]) {
-                i++;
+        this.matrix = [];
+        this.input = [];
+        this.alphabet = new Map();
+        this.statements = new Map();
+        this.getStatementsFromNodes = function (nodes) {
+            for (var i = 0; i < nodes.length; i++) {
+                _this.statements.set(nodes[i].id, { isAdmit: nodes[i].isAdmit, idLogic: i });
             }
-            return i;
         };
-        this.firstMatchStatement = function (current) {
-            var i = 0;
-            while (current !== _this.statements[i]) {
-                i++;
+        this.getAlphabetFromEdges = function (edges) {
+            var alphabetSet = new Set;
+            for (var i_1 = 0; i_1 < edges.length; i_1++) {
+                alphabetSet.add(edges[i_1].value);
             }
-            return i;
+            var i = 0;
+            alphabetSet.forEach(function (value) {
+                _this.alphabet.set(value, i);
+                i++;
+            });
+        };
+        this.createMatrix = function (edges) {
+            for (var i = 0; i < _this.statements.size; i++) {
+                _this.matrix[i] = [];
+                for (var j = 0; j < _this.alphabet.size; j++) {
+                    _this.matrix[i][j] = exports.eof;
+                }
+            }
+            for (var i = 0; i < edges.length; i++) {
+                var statementNumberFrom = _this.statements.get(edges[i].from).idLogic;
+                var alphabetSymbolNumber = _this.alphabet.get(edges[i].value);
+                var statementNumberTo = _this.statements.get(edges[i].to);
+                _this.matrix[statementNumberFrom][alphabetSymbolNumber] = statementNumberTo;
+                //console.log(this.statements.get(edges[i].from).idLogic, this.alphabet.get(edges[i].value), '<->', edges[i].value, this.statements.get(edges[i].to))
+            }
+        };
+        this.getTransformedInput = function (input) {
+            input.forEach(function (value) {
+                _this.input.push({ idLogic: _this.alphabet.get(value), value: value });
+                console.log(value, _this.alphabet.get(value));
+            });
         };
         this.isAdmit = function () {
-            var current = _this.startStatement;
+            var current = _this.statements.get(_this.startStatement.id);
             var oldCurrent = current;
             var l = 0;
-            var i = 0;
+            var i = current.idLogic;
             var j = -1; //now we see at left column of table of def statements
             console.log('NOW in', current, 'start statement');
             while (l < _this.input.length) {
-                j = _this.firstMatchValue(_this.input[l]);
+                j = _this.input[l].idLogic;
                 if (_this.matrix[i][j] === exports.eof) {
                     console.log('FUBAR Aoutomata was stoped in ', oldCurrent, 'because string in matrix has only EOF values (noway from this statement)', ' in: ', i, ' ', j);
                     return oldCurrent.isAdmit;
@@ -35,26 +67,43 @@ var DFA = /** @class */ (function () {
                 oldCurrent = current;
                 current = _this.matrix[i][j];
                 console.log('NOW in', current, ' ~ ', i, ' ', j);
-                i = _this.firstMatchStatement(_this.matrix[i][j]);
+                i = _this.matrix[i][j].idLogic;
                 l++;
             }
             console.log('Aoutomata was stoped in ', current, ' ~ ', i, ' ', j);
             return current.isAdmit;
         };
-        this.statements = statements;
-        this.input = input;
-        this.matrix = matrix;
-        this.alphabet = alphabet;
-        this.startStatement = statements[0];
+        var edges = graph.edges.sort(function (a, b) { return a.from - b.from; });
+        //console.log(edges)
+        this.getAlphabetFromEdges(edges);
+        //console.log(this.alphabet)
+        this.getStatementsFromNodes(graph.nodes);
+        //console.log(this.statements)
+        this.createMatrix(edges);
+        //console.log(this.matrix)
+        this.startStatement = startStatement;
+        this.getTransformedInput(input);
     }
     return DFA;
 }());
 exports.DFA = DFA;
-var q0 = { id: 0, isAdmit: true };
-var q1 = { id: 1, isAdmit: false };
+var q0 = { id: 0, isAdmit: true, idLogic: 0 };
+var q1 = { id: 1, isAdmit: false, idLogic: 1 };
 var matrix = [
     [q1, q1],
     [q0, q0]
 ];
-var dfa = new DFA([q0, q1], matrix, [0, 1, 1, 1, 1], [0, 1]);
-console.log(dfa.isAdmit());
+//let dfa = new DFA([q0, q1] ,matrix, [{idLogic: 0, value: '0'},  {idLogic: 1, value: '0'}], [{idLogic: 0, value: '0'}, {idLogic: 1, value: '1'}])
+var dfa = new DFA({ id: 0, isAdmit: true }, {
+    nodes: [
+        { id: 0, isAdmit: true },
+        { id: 44, isAdmit: false },
+        { id: 88, isAdmit: false }
+    ],
+    edges: [
+        { from: 0, to: 44, value: '0' },
+        { from: 44, to: 88, value: '0' },
+        { from: 88, to: 88, value: 'a' }
+    ]
+}, ['0', '0', 'a']);
+dfa.isAdmit();
--- app/src/Logic/DFA.ts
+++ app/src/Logic/DFA.ts
@@ -1,52 +1,92 @@
-export type statement = {
-    id: number
-    isAdmit: boolean;
-}
-export type elementOfAlphabet = number // does we need in func alphabet -> hashedAlphabet  (+ use in input for any element) <T>
+import {edgeCore, graphCore, nodeCore} from "./IGraphTypes";
 
-export const eof: statement = {id: -1, isAdmit: false}
+export interface statement extends nodeCore {
+    idLogic: number
+}
+export type elementOfAlphabet = {
+    value: string
+    idLogic: number
+}
+
+export const eof: statement = { isAdmit: false, idLogic: -1 }
 
 export class DFA {
 
-    private readonly matrix: statement[][]
-    private readonly input: elementOfAlphabet[]
-    private readonly alphabet: elementOfAlphabet[]
-    private readonly statements: statement[]
+    private readonly matrix: statement[][] = []
+    private readonly input: elementOfAlphabet[] = []
+    private readonly alphabet = new Map()
+    private readonly statements = new Map()
     private readonly startStatement
 
-    constructor(statements: statement[], matrix: statement[][], input: elementOfAlphabet[] , alphabet: elementOfAlphabet[]) {
-        this.statements = statements
-        this.input = input
-        this.matrix = matrix
-        this.alphabet = alphabet
-        this.startStatement = statements[0]
+    private getStatementsFromNodes = (nodes: nodeCore[]) : void => {
+        for (let i = 0; i < nodes.length; i++) {
+            this.statements.set(nodes[i].id, {isAdmit: nodes[i].isAdmit, idLogic: i})
+        }
     }
 
-    private firstMatchValue = (current: elementOfAlphabet) : number => { //hash?
-        let i = 0;
-        while (current !== this.alphabet[i]) {
-            i++;
+    private getAlphabetFromEdges = (edges: edgeCore[]) : void => {
+        let alphabetSet: Set<string> = new Set
+        for (let i = 0; i < edges.length; i++) {
+            alphabetSet.add(edges[i].value)
         }
-        return i
+        let i = 0
+        alphabetSet.forEach(value => {
+            this.alphabet.set(value, i)
+            i++
+        })
     }
 
-    private firstMatchStatement = (current: statement) : number => { //hash?
-        let i = 0;
-        while (current !== this.statements[i]) {
-            i++;
+    private createMatrix = (edges: edgeCore[]) : void => {
+        for (let i = 0; i < this.statements.size; i++) {
+            this.matrix[i] = []
+            for (let j = 0; j < this.alphabet.size; j++) {
+                this.matrix[i][j] = eof
+            }
         }
-        return i
+        for (let i = 0; i < edges.length; i++) {
+            let statementNumberFrom = this.statements.get(edges[i].from).idLogic
+            let alphabetSymbolNumber = this.alphabet.get(edges[i].value)
+            let statementNumberTo = this.statements.get(edges[i].to)
+            this.matrix[statementNumberFrom][alphabetSymbolNumber] = statementNumberTo
+            //console.log(this.statements.get(edges[i].from).idLogic, this.alphabet.get(edges[i].value), '<->', edges[i].value, this.statements.get(edges[i].to))
+        }
+    }
+
+    private getTransformedInput = (input: string[]) : void => {
+        input.forEach(value => {
+            this.input.push({idLogic: this.alphabet.get(value), value: value})
+            console.log(value, this.alphabet.get(value))
+        })
+    }
+
+    constructor(startStatement: nodeCore, graph: graphCore, input: string[] /*statements: statement[], matrix: statement[][], input: elementOfAlphabet[] , alphabet: elementOfAlphabet[]*/) {
+        //this.statements = statements
+        //this.input = input
+        //this.matrix = matrix
+        //this.alphabet = alphabet
+        //this.startStatement = statements[0]
+
+        let edges = graph.edges.sort((a, b) => a.from - b.from)
+        //console.log(edges)
+        this.getAlphabetFromEdges(edges)
+        //console.log(this.alphabet)
+        this.getStatementsFromNodes(graph.nodes)
+        //console.log(this.statements)
+        this.createMatrix(edges)
+        //console.log(this.matrix)
+        this.startStatement = startStatement
+        this.getTransformedInput(input)
     }
 
     public isAdmit = () : boolean => {
-        let current: statement = this.startStatement
+        let current: statement = this.statements.get(this.startStatement.id)
         let oldCurrent = current
         let l = 0
-        let i = 0
+        let i = current.idLogic
         let j = -1 //now we see at left column of table of def statements
         console.log('NOW in', current, 'start statement')
         while (l < this.input.length) {
-            j = this.firstMatchValue(this.input[l])
+            j = this.input[l].idLogic
             if (this.matrix[i][j] === eof) {
                 console.log('FUBAR Aoutomata was stoped in ', oldCurrent, 'because string in matrix has only EOF values (noway from this statement)', ' in: ', i, ' ', j)
                 return oldCurrent.isAdmit
@@ -54,7 +94,7 @@ export class DFA {
             oldCurrent = current
             current = this.matrix[i][j]
             console.log('NOW in', current, ' ~ ', i, ' ', j)
-            i = this.firstMatchStatement(this.matrix[i][j])
+            i = this.matrix[i][j].idLogic
             l++
         }
         console.log('Aoutomata was stoped in ', current, ' ~ ', i, ' ', j)
@@ -62,21 +102,26 @@ export class DFA {
     }
 
 }
-
-
-
-
-
-
-
-let q0: statement = {id: 0, isAdmit: true}
-let q1: statement = {id: 1, isAdmit: false}
+/*
+let q0: statement = {id: 0, isAdmit: true, idLogic: 0}
+let q1: statement = {id: 1, isAdmit: false, idLogic: 1}
 
 let matrix: statement[][] = [
     [q1,q1],
     [q0,q0]
+]*/
+//let dfa = new DFA([q0, q1] ,matrix, [{idLogic: 0, value: '0'},  {idLogic: 1, value: '0'}], [{idLogic: 0, value: '0'}, {idLogic: 1, value: '1'}])
+let dfa = new DFA( {id: 0, isAdmit: true},
+    {
+        nodes: [
+            {id: 0, isAdmit: true},
+            {id: 44, isAdmit: false},
+            {id: 88, isAdmit: false}
+        ],
+    edges: [
+        {from: 0, to: 44, value: '0'},
+        {from: 44, to: 88, value: '0'},
+        {from: 88, to: 88, value: 'a'}
 ]
-let dfa = new DFA([q0, q1] ,matrix, [0,  1, 1,1,1], [0,1])
-
-console.log(dfa.isAdmit())
-
+}, ['0','0','a'])
+dfa.isAdmit()
