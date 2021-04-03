a4a3f1ca01d3ad8a2504eba541b4fd24ba9498b2
andrew <a.oleyn1kov@outlook.com>
2021-04-03 15:05:52 +0300

    Exception fix

--- app/src/Logic/DFA.ts
+++ app/src/Logic/DFA.ts
@@ -1,5 +1,6 @@
 import {GraphCore, NodeCore} from "./IGraphTypes";
 import {NFA} from "./NFA";
+import {Step} from "./Types";
 
 export class DFA extends NFA {
     private nfa: NFA
@@ -8,8 +9,38 @@ export class DFA extends NFA {
         this.nfa = new NFA(graph, startStatement, input)
         this.setInput(input)
         if (this.nfa.run().nodes.length > 1) {
-            throw "Is not determenistic"
+            throw new Error("Is not determenistic")
         }
     }
 
 }
+let toSet = (str: string[]) => {
+    let set: Set<string> = new Set()
+    for (let i = 0; i < str.length; i++) {
+        set.add(str[i])
+    }
+    return set;
+}
+
+let testFunc = (nfa: NFA) : Step => {
+    for (let i = 0; i < nfa.input.length; i++) {
+        nfa.step()
+    }
+    return nfa.getTrendyNode()
+}
+
+
+    let dfa = new DFA(
+        {
+            nodes: [
+                {id: 1, isAdmit: false},
+                {id: 2, isAdmit: false},
+                {id: 3, isAdmit: true},
+            ],
+            edges: [
+                {from: 1, to: 1, transitions: toSet(['0', '1'])},
+                {from: 1, to: 2, transitions: toSet(['0'])},
+                {from: 2, to: 3, transitions: toSet(['0'])}
+            ]
+        }, {id: 1, isAdmit: false}, ['0'])
+
