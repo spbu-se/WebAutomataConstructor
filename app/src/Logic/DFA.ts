5e542fd9c85682d4c420192c494472387f848308
andrew <a.oleyn1kov@outlook.com>
2021-04-03 15:06:21 +0300

    Exception fix

--- app/src/Logic/DFA.ts
+++ app/src/Logic/DFA.ts
@@ -14,33 +14,3 @@ export class DFA extends NFA {
     }
 
 }
-let toSet = (str: string[]) => {
-    let set: Set<string> = new Set()
-    for (let i = 0; i < str.length; i++) {
-        set.add(str[i])
-    }
-    return set;
-}
-
-let testFunc = (nfa: NFA) : Step => {
-    for (let i = 0; i < nfa.input.length; i++) {
-        nfa.step()
-    }
-    return nfa.getTrendyNode()
-}
-
-
-    let dfa = new DFA(
-        {
-            nodes: [
-                {id: 1, isAdmit: false},
-                {id: 2, isAdmit: false},
-                {id: 3, isAdmit: true},
-            ],
-            edges: [
-                {from: 1, to: 1, transitions: toSet(['0', '1'])},
-                {from: 1, to: 2, transitions: toSet(['0'])},
-                {from: 2, to: 3, transitions: toSet(['0'])}
-            ]
-        }, {id: 1, isAdmit: false}, ['0'])
-
