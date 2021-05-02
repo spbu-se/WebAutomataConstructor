f28da69aad67ef1cbabcba472d26fd83f42e837e
ususucsus <usarti01@gmail.com>
2021-05-02 21:53:20 +0300

    Updates

---
+++ precache-manifest.2d67abb40c19f190afe3f02b25c8d3cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98358d268cdf98ff674f3490fda27f9d",
+    "url": "/WebAutomataConstructor/index.html"
+  },
+  {
+    "revision": "3039d23ba71c38bf0f9d",
+    "url": "/WebAutomataConstructor/static/css/main.72a4205c.chunk.css"
+  },
+  {
+    "revision": "3260f06a880908615dd6",
+    "url": "/WebAutomataConstructor/static/js/2.3c4a0d64.chunk.js"
+  },
+  {
+    "revision": "3039d23ba71c38bf0f9d",
+    "url": "/WebAutomataConstructor/static/js/main.ab509909.chunk.js"
+  },
+  {
+    "revision": "201916dba962cebc2ad6",
+    "url": "/WebAutomataConstructor/static/js/runtime~main.f485f77d.js"
+  }
+]);
