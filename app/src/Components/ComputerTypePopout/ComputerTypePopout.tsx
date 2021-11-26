f43a4478c244ebf0f8b5791cfe1130720046b6a3
ususucsus <usarti01@gmail.com>
2021-11-26 23:09:22 +0300

    rewrote computer type popout to functional component; refactor

--- app/src/Components/ComputerTypePopout/ComputerTypePopout.tsx
+++ app/src/Components/ComputerTypePopout/ComputerTypePopout.tsx
@@ -1,170 +1,173 @@
-import React, {AllHTMLAttributes} from "react";
-import "./ComputerTypePopout.css";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import {computersInfo} from "../../utils";
+import React, {useEffect, useState} from "react";
+
 import {ComputerType, graph} from "../../react-graph-vis-types";
-import {Button, Dialog, DialogContent, Paper} from "@mui/material";
+import {computersInfo} from "../../utils";
+
 import BrowserSavesManager from "../../SavesManager/BrowserSavesManager";
 import {SaveMeta} from "../../SavesManager/Save";
+
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
 import Typography from "@mui/material/Typography";
 
-interface ComputerTypePopoutProps extends AllHTMLAttributes<HTMLElement> {
-    changeComputerType: (computerType: null | ComputerType, graph: graph | null) => void,
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+import "./ComputerTypePopout.css";
+
+export interface ComputerTypePopoutProps {
     open: boolean,
     onClose: () => void
+
+    changeComputerType: (computerType: null | ComputerType, graph: graph | null) => void,
 }
 
-interface ComputerTypePopoutState {
-    savesMeta: SaveMeta[],
-}
+export const ComputerTypePopout: React.FunctionComponent<ComputerTypePopoutProps> = (
+    {
+        open,
+        onClose,
+        changeComputerType
+    }) => {
+    const browserSavesManager = new BrowserSavesManager();
 
-class ComputerTypePopout extends React.Component<ComputerTypePopoutProps, ComputerTypePopoutState> {
-    constructor(props: ComputerTypePopoutProps) {
-        super(props);
-
-        this.state = {
-            savesMeta: [],
-        }
-    }
-
-    async componentDidMount() {
-        this.setState({savesMeta: await (new BrowserSavesManager()).getSavesMeta()});
-    }
-
-    onSaveClicked = async (saveMeta: SaveMeta) => {
+    const onSaveClicked = async (saveMeta: SaveMeta) => {
         let save = await (new BrowserSavesManager()).getSave(saveMeta);
 
         if (save) {
-            this.props.changeComputerType(save.save.type, save.save.graph);
+            changeComputerType(save.save.type, save.save.graph);
         }
 
-        this.props.onClose();
+        onClose();
     }
 
-    onCreateTemplateClicked = (type: ComputerType) => {
-        this.props.changeComputerType(type, null);
-        this.props.onClose();
+    const onCreateTemplateClicked = (type: ComputerType) => {
+        changeComputerType(type, null);
+        onClose();
     }
 
-    onCreateEmptyClicked = (type: ComputerType) => {
-        this.props.changeComputerType(type, {nodes: [], edges: []});
-        this.props.onClose();
+    const onCreateEmptyClicked = (type: ComputerType) => {
+        changeComputerType(type, {nodes: [], edges: []});
+        onClose();
     }
 
-    render() {
-        const {changeComputerType, open, onClose, className, style, ...restProps} = this.props;
-        const {savesMeta} = this.state;
+    const updateSavesMeta = async () => {
+        setSavesMeta(await browserSavesManager.getSavesMeta())
+    }
 
-        return (
+    const [savesMeta, setSavesMeta] = useState<SaveMeta[]>([]);
 
-            <Dialog open={open} onClose={onClose} maxWidth="md">
+    useEffect(() => {
+        updateSavesMeta();
+    }, [open]);
 
-                <DialogTitle>
-                    <p>Симулятор автоматов</p>
-                </DialogTitle>
 
-                <DialogContent>
-                    <div className="computer-type-popout__sections">
-                        <div className="computer-type-popout__section">
-                            <Typography
-                                variant="subtitle1"
-                                gutterBottom
-                            >
-                                Сохранения
-                            </Typography>
+    return (
+        <Dialog open={open} onClose={onClose} maxWidth="md">
+            <DialogTitle>
+                <p>Симулятор автоматов</p>
+            </DialogTitle>
 
-                            <div className="computer-type-popout__savings__container">
-                                {
-                                    savesMeta.map(saveMeta => (
-                                        <Paper
-                                            key={saveMeta.id}
-                                            className="computer-type-popout__saving"
-                                            variant="outlined"
-                                            onClick={() => this.onSaveClicked(saveMeta)}
-                                        >
-                                            {saveMeta.name}
-                                        </Paper>
-                                    ))
-                                }
-                            </div>
+            <DialogContent>
+                <div>
+                    <div className="computer-type-popout__section">
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                        >
+                            Сохранения
+                        </Typography>
+
+                        <div className="computer-type-popout__savings__container">
+                            {
+                                savesMeta.map(saveMeta => (
+                                    <Paper
+                                        key={saveMeta.id}
+                                        className="computer-type-popout__saving"
+                                        variant="outlined"
+                                        onClick={() => onSaveClicked(saveMeta)}
+                                    >
+                                        {saveMeta.name}
+                                    </Paper>
+                                ))
+                            }
                         </div>
+                    </div>
 
-                        <div className="computer-type-popout__section">
-                            <Typography
-                                variant="subtitle1"
-                                gutterBottom
-                            >
-                                Шаблоны
-                            </Typography>
+                    <div className="computer-type-popout__section">
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                        >
+                            Шаблоны
+                        </Typography>
 
-                            <div className="computer-type-popout__templates__container">
-                                {
-                                    Object.entries(computersInfo).map((entry, index) =>
-                                        <div key={index} className="computer-type-popout__templates__card">
-                                            <img className="computer-type-popout__templates__card__preview"
-                                                 src={`media/images/${entry[1].preview}`}
-                                                 alt={`${entry[1].name} preview`}
-                                            />
-                                            <div className="computer-type-popout__templates__card__name">
-                                                {entry[1].name}
-                                            </div>
-                                            <div className="computer-type-popout__templates__card__description">
-                                                {entry[1].description}
-                                            </div>
-
-                                            <div className="computer-type-popout__templates__card__create-buttons">
-                                                <Button
-                                                    className="computer-type-popout__templates__card__create-button"
-                                                    variant="contained"
-                                                    color="primary"
-                                                    size="small"
-                                                    fullWidth
-                                                    onClick={() => this.onCreateTemplateClicked(entry[0] as ComputerType)}
-                                                >
-                                                    Создать
-                                                </Button>
-
-                                                <Button
-                                                    className="computer-type-popout__templates__card__create-button"
-                                                    color="primary"
-                                                    size="small"
-                                                    fullWidth
-                                                    onClick={() => this.onCreateEmptyClicked(entry[0] as ComputerType)}
-                                                >
-                                                    Создать пустым
-                                                </Button>
-                                            </div>
-
+                        <div className="computer-type-popout__templates__container">
+                            {
+                                Object.entries(computersInfo).map((entry, index) =>
+                                    <div key={index} className="computer-type-popout__templates__card">
+                                        <img className="computer-type-popout__templates__card__preview"
+                                             src={`media/images/${entry[1].preview}`}
+                                             alt={`${entry[1].name} preview`}
+                                        />
+                                        <div className="computer-type-popout__templates__card__name">
+                                            {entry[1].name}
                                         </div>
-                                    )
-                                }
-                            </div>
+                                        <div className="computer-type-popout__templates__card__description">
+                                            {entry[1].description}
+                                        </div>
+
+                                        <div className="computer-type-popout__templates__card__create-buttons">
+                                            <Button
+                                                className="computer-type-popout__templates__card__create-button"
+                                                variant="contained"
+                                                color="primary"
+                                                size="small"
+                                                fullWidth
+                                                onClick={() => onCreateTemplateClicked(entry[0] as ComputerType)}
+                                            >
+                                                Создать
+                                            </Button>
+
+                                            <Button
+                                                className="computer-type-popout__templates__card__create-button"
+                                                color="primary"
+                                                size="small"
+                                                fullWidth
+                                                onClick={() => onCreateEmptyClicked(entry[0] as ComputerType)}
+                                            >
+                                                Создать пустым
+                                            </Button>
+                                        </div>
+
+                                    </div>
+                                )
+                            }
                         </div>
+                    </div>
 
-                        <div className="computer-type-popout__section">
-                            <Typography
-                                variant="subtitle1"
-                                gutterBottom
-                            >
-                                Проект
-                            </Typography>
+                    <div className="computer-type-popout__section">
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                        >
+                            Проект
+                        </Typography>
 
-                            <div className="computer-type-popout__credits__line">
-                                <div className="computer-type-popout__credits__line__icon">
-                                    <GitHubIcon/>
-                                </div>
-                                <div className="computer-type-popout__credits__line__link">
-                                    <a href="https://github.com/spbu-se/WebAutomataConstructor">https://github.com/spbu-se/WebAutomataConstructor</a>
-                                </div>
+                        <div className="computer-type-popout__credits__line">
+                            <div className="computer-type-popout__credits__line__icon">
+                                <GitHubIcon/>
+                            </div>
+                            <div className="computer-type-popout__credits__line__link">
+                                <a href="https://github.com/spbu-se/WebAutomataConstructor">https://github.com/spbu-se/WebAutomataConstructor</a>
                             </div>
                         </div>
                     </div>
-                </DialogContent>
-            </Dialog>
-        );
-        // {/*</PopoutWrapper>*/
-    }
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
 }
 
 export default ComputerTypePopout;
