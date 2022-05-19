2ad3d18fe80decb014e3b4750ea571226e1c7d27
Andrew <a.oleyn1kov@outlook.com>
2022-05-19 03:49:20 +0300

    Updates

---
+++ precache-manifest.a8c3531495fc630c151aa1e8679dd8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6bd6d289aededa3dfbde142c08921b",
+    "url": "/WebAutomataConstructor/index.html"
+  },
+  {
+    "revision": "97c3650fece7ef14fb96",
+    "url": "/WebAutomataConstructor/static/css/main.3bc6e941.chunk.css"
+  },
+  {
+    "revision": "6c55dbde9e4bed2cb217",
+    "url": "/WebAutomataConstructor/static/js/2.550a05b9.chunk.js"
+  },
+  {
+    "revision": "97c3650fece7ef14fb96",
+    "url": "/WebAutomataConstructor/static/js/main.f5ca1230.chunk.js"
+  },
+  {
+    "revision": "201916dba962cebc2ad6",
+    "url": "/WebAutomataConstructor/static/js/runtime~main.f485f77d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WebAutomataConstructor/precache-manifest.8a7b2d34d4cfcb1a9959a97c09228955.js"
+  "/WebAutomataConstructor/precache-manifest.a8c3531495fc630c151aa1e8679dd8e2.js"
 );
 
 self.addEventListener('message', (event) => {
