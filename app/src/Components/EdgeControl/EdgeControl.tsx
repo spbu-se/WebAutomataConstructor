acd935a77ad3440854eadd38203460f5a0d80404
Andrew <a.oleyn1kov@outlook.com>
2022-05-19 04:47:49 +0300

    help text

--- app/src/Components/EdgeControl/EdgeControl.tsx
+++ app/src/Components/EdgeControl/EdgeControl.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {ComputerType, edge} from "../../react-graph-vis-types";
-import {getTransitionsTitles, transitionsToLabel} from "../../utils";
+import { ComputerType, edge } from "../../react-graph-vis-types";
+import { getTransitionsTitles, transitionsToLabel } from "../../utils";
 import ControlWrapper from "../ControlWrapper/ControlWrapper";
 import Button from "@mui/material/Button";
 import "./EdgeControl.css";
 import Transition from "./Transition/Transition";
 import EditIcon from '@mui/icons-material/Edit';
 import TextField from "@mui/material/TextField";
-import {withComputerType} from "../../hoc";
-import {EPS} from "../../Logic/Computer";
-import {Move, TransitionParams} from "../../Logic/IGraphTypes";
+import { withComputerType } from "../../hoc";
+import { EPS } from "../../Logic/Computer";
+import { Move, TransitionParams } from "../../Logic/IGraphTypes";
 import { TextareaAutosize } from "@mui/material";
 
 
@@ -21,7 +21,7 @@ interface EdgeControlProps {
     reinitComputer: (() => void)
 }
 
-interface Rule{
+interface Rule {
     id: number
     value: string
 }
@@ -33,7 +33,7 @@ interface EdgeControlState {
     displayedTransitionText: string,
     activeTransition: TransitionParams[] | null,
     editMode: boolean,
-    countRules?: number, 
+    countRules?: number,
     rules: Rule[],
 }
 
@@ -50,7 +50,7 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
             editMode: false,
             countRules: 1,
             rules: [
-                {id: 1, value: ' '}
+                { id: 1, value: ' ' }
             ]
         }
     }
@@ -71,21 +71,22 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
 
     selectTransition = (transition: TransitionParams[] | null): void => {
         if (this.state.activeTransition === transition) {
-            this.setState({activeTransition: null});
+            this.setState({ activeTransition: null });
         } else {
-            this.setState({activeTransition: transition});
+            this.setState({ activeTransition: transition });
         }
     }
 
     changeTransitions = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
-        this.setState({transitionsText: value});
+        this.setState({ transitionsText: value });
 
         let accumulator: {
             fst: string | undefined,
             snd: string | undefined,
             trd: string | undefined,
-            fth: string | undefined }[] = []
+            fth: string | undefined
+        }[] = []
         let acc: TransitionParams[] = []
 
         if (this.props.computerType === "tm") {
@@ -110,19 +111,19 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
                     accumulator.push({ fst: EPS, snd: fst, trd: trd.join(':'), fth: fth })
                 })
 
-        } 
+        }
         if (this.props.computerType === "mealy" || this.props.computerType === "dmealy") {
             this.state.transitionsText
-            .split('')
-            .filter(x => x !== " " && x !== "\n")
-            .join('')
-            .split(";")
-            .forEach(value => {
-                let tmp = value.split("|")
-                let fst = tmp.shift()
-                let snd = tmp.shift()
-                accumulator.push({ fst: fst, snd: snd, trd: undefined, fth: undefined })
-            })
+                .split('')
+                .filter(x => x !== " " && x !== "\n")
+                .join('')
+                .split(";")
+                .forEach(value => {
+                    let tmp = value.split("|")
+                    let fst = tmp.shift()
+                    let snd = tmp.shift()
+                    accumulator.push({ fst: fst, snd: snd, trd: undefined, fth: undefined })
+                })
 
             accumulator.forEach(value => {
                 if (value.fst !== undefined) {
@@ -134,7 +135,7 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
                     )
                 }
             })
-        } 
+        }
         else {
             this.state.transitionsText
                 .split('')
@@ -172,7 +173,8 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
         /////
         // await this.props.reinitComputer()
         ///
-        this.setState({transitionsText: value
+        this.setState({
+            transitionsText: value
             , transitions: transitions
         }, () => this.props.reinitComputer());
     }
@@ -184,26 +186,26 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
         await this.props.reinitComputer()
 
     }
-    
+
     private addInstruction(value: string) {
-        let newRow = {id: this.state.rules.length, value: value};
-        this.setState({rules: this.state.rules.concat(newRow)});
+        let newRow = { id: this.state.rules.length, value: value };
+        this.setState({ rules: this.state.rules.concat(newRow) });
     }
-    
+
     changeEditMode = () => {
-         this.setState({editMode: !this.state.editMode});
-         this.updateTransitions();
+        this.setState({ editMode: !this.state.editMode });
+        this.updateTransitions();
 
     }
-    
+
     updateTransitions = async () => {
         let accumulator: {
             fst: string | undefined,
             snd: string | undefined,
             trd: string | undefined,
-            fth: string | undefined 
+            fth: string | undefined
             out: string | undefined
-        } [] = []
+        }[] = []
 
         if (this.props.computerType === "tm") {
             this.state.transitionsText
@@ -229,20 +231,20 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
                     accumulator.push({ fst: EPS, snd: fst, trd: trd.join(':'), fth: fth, out: undefined })
                 })
 
-        } 
+        }
         if (this.props.computerType === "mealy" || this.props.computerType === "dmealy") {
             this.state.transitionsText
-            .split('')
-            .filter(x => x !== " " && x !== "\n")
-            .join('')
-            .split(";")
-            .forEach(value => {
-                let tmp = value.split("|")
-                let fst = tmp.shift()
-                let snd = tmp.shift()
-                accumulator.push({ fst: fst, snd: undefined, trd: undefined, fth: undefined, out: snd })
-            })
-        } 
+                .split('')
+                .filter(x => x !== " " && x !== "\n")
+                .join('')
+                .split(";")
+                .forEach(value => {
+                    let tmp = value.split("|")
+                    let fst = tmp.shift()
+                    let snd = tmp.shift()
+                    accumulator.push({ fst: fst, snd: undefined, trd: undefined, fth: undefined, out: snd })
+                })
+        }
         else {
             this.state.transitionsText
                 .split('')
@@ -270,7 +272,7 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
                         stackPush: value.trd?.split(':').map(value => value === 'eps' ? EPS : value),
                         move: value.fth === 'L' ? Move.L : value.fth === 'R' ? Move.R : undefined,
                         output: value.out,
-                        
+
                     }
                 )
             }
@@ -293,10 +295,30 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
             transitions.delete(this.state.activeTransition);
 
             this.props.changeEdgeTransitions(this.props.edge.id!, transitions);
-            this.setState({transitions: transitions, transitionsText: getTransitionsTitles(transitions, this.props.computerType)});
+            this.setState({ transitions: transitions, transitionsText: getTransitionsTitles(transitions, this.props.computerType) });
         }
     }
 
+    helperText = () => {
+        switch (this.props.computerType) {
+            case "dfa":
+            case "nfa":
+            case "nfa-eps":
+            case "moore":
+            case "dmoore":
+                return 'Список символов или "eps" через точку с запятой';
+            case "dmealy":
+            case "mealy":
+                return '"f | n;", "eps" для ε'
+            case "dpda":
+            case "pda":
+                return '"a, a | a:Z0;", "eps" для ε. На дне стека "Z0"'
+            case "tm":
+                return '"a | a>R;", eps для ε. "R" - вправо, "L" - влево'
+            default: 
+                return '';
+        }
+    }
 
 
     render() {
@@ -305,23 +327,23 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
                 <div className="edge-control__container">
                     <div className="edge-control__item edge-control__transitions">
                         {
-                                <TextField
-                                    id="filled-multiline-static"
-                                    multiline
-                                    rows={3}
-                                    variant="standard"
-                                    label="Переходы"
-                                    size="small"
-                                    value={this.state.transitionsText}
-                                    onChange={this.changeTransitions}
-                                    helperText={this.props.computerType === "nfa-eps" || "pda"|| "dpda" ? 'Список символов или "eps" через запятую' : "Список символов через запятую"}
-                                    onBlur={this.updateTransitions}
-                                />
+                            <TextField
+                                id="filled-multiline-static"
+                                multiline
+                                rows={3}
+                                variant="standard"
+                                label="Переходы"
+                                size="small"
+                                value={this.state.transitionsText}
+                                onChange={this.changeTransitions}
+                                helperText={this.helperText()}
+                                onBlur={this.updateTransitions}
+                            />
                         }
 
                         <div className="edge-control__edit-transitions"
-                             onClick={this.changeEditMode}>
-                            <EditIcon/>
+                            onClick={this.changeEditMode}>
+                            <EditIcon />
                         </div>
 
                     </div>
@@ -337,8 +359,8 @@ class EdgeControl extends React.Component<EdgeControlProps, EdgeControlState> {
 
                 </div>
             </ControlWrapper>
-            );
-        }
+        );
+    }
 }
 
 export default withComputerType(EdgeControl);
