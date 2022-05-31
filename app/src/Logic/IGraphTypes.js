2ead760b7c22cccbdfc2f4c66d3b44fd2fb5e154
Andrew <a.oleyn1kov@outlook.com>
2022-05-31 19:55:32 +0300

    rm js files

--- app/src/Logic/IGraphTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Move;
-(function (Move) {
-    Move[Move["L"] = 0] = "L";
-    Move[Move["R"] = 1] = "R";
-})(Move = exports.Move || (exports.Move = {}));
